79ea878cb536af28d1098c2a135930fad0dd9292
Alina Hryn <alin.grin2000@gmail.com>
2020-12-12 18:52:15 +0200

    split file lines

--- ruby-formatter/codeAnalizer/codeParser.py
+++ ruby-formatter/codeAnalizer/codeParser.py
@@ -1,2 +1,15 @@
+from codeAnalizer.cases import get_list_from_line
+
+
 class CodeParser:
-    pass
+    def __init__(self, lines):
+        self.lines = lines
+        self.lexemes = []
+        self.make_line_to_lexems()
+
+    def make_line_to_lexems(self):
+        for line in self.lines:
+            self.lexemes.append(get_list_from_line(line))
+            # self.tokens.append(line_tokens)
+        print(self.lexemes)
+
