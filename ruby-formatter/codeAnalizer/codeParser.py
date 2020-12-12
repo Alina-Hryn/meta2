ddd9d0032abf18555a8f6efddd6d30a3e2091574
Alina Hryn <alin.grin2000@gmail.com>
2020-12-13 01:27:24 +0200

    delete strings

--- ruby-formatter/codeAnalizer/codeParser.py
+++ ruby-formatter/codeAnalizer/codeParser.py
@@ -14,11 +14,13 @@ class CodeParser:
         print(self.lines)
         for line in self.lines:
             self.lexemes.append(get_list_from_line(line))
+            print(line)
         print(self.lexemes)
 
     def remove_comments(self):
-        self.file_string = re.sub(r"=begin(.|\n)+?=end", "", self.file_string)
-        self.file_string = re.sub(r"#.+", "", self.file_string)
+        self.file_string = re.sub(r"('.*')|(\".*\")", "'STRING'", self.file_string)
+        self.file_string = re.sub(r"=begin(.|\n)+?=end", "'MULTILINE_COMMENT'", self.file_string)
+        self.file_string = re.sub(r"#.+", "'ONE_LINE_COMMENT'", self.file_string)
 
     def make_list_from_file(self):
         self.lines = self.file_string.split('\n')
