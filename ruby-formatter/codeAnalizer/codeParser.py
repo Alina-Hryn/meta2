143274b6417978cde95268ec7bfe5fbf7f19a0e8
Alina Hryn <alin.grin2000@gmail.com>
2020-12-13 04:39:33 +0200

    add checking lexeme

--- ruby-formatter/codeAnalizer/codeParser.py
+++ ruby-formatter/codeAnalizer/codeParser.py
@@ -1,12 +1,14 @@
 from codeAnalizer.cases import get_list_from_line
 import re
 
+
 class CodeParser:
     def __init__(self, file_string, lines):
         self.file_string = file_string
         self.lines = lines
         self.lexemes = []
         self.make_lexems()
+        self.tokens = []
 
     def make_lexems(self):
         self.remove_comments()
@@ -14,13 +16,27 @@ class CodeParser:
         print(self.lines)
         for line in self.lines:
             self.lexemes.append(get_list_from_line(line))
+            self.search_incorrect_lexeme(self.lexemes[-1])
             print(line)
         print(self.lexemes)
+
+    def search_incorrect_lexeme(self, line):
+        if line[0].upper() == 'def'.upper():
+            print("\t \t \t\t \t \t DEF!")
+        elif line[0].upper == 'class'.upper():
+            print('\t \t \t\t \t \t CLASS')
+        elif line[0][0] == '@':
+            print('\t \t \t\t \t \t @')
+        elif line[0][0] == '$':
+            print('\t \t \t\t \t \t $')
+        else:
+            print('\t \t \t' + line[0])
 
     def remove_comments(self):
         self.file_string = re.sub(r"('.*')|(\".*\")", "'STRING'", self.file_string)
         self.file_string = re.sub(r"=begin(.|\n)+?=end", "'MULTILINE_COMMENT'", self.file_string)
         self.file_string = re.sub(r"#.+", "'ONE_LINE_COMMENT'", self.file_string)
+        self.file_string = re.sub(";", "\n", self.file_string)
 
     def make_list_from_file(self):
         self.lines = self.file_string.split('\n')
