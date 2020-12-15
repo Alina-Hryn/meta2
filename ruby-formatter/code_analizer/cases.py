5ffa7c64444c8ec79f1a84e04e45355c97311c31
Alina Hryn <alin.grin2000@gmail.com>
2020-12-15 03:20:54 +0200

    add annotations dict

--- ruby-formatter/code_analizer/cases.py
+++ ruby-formatter/code_analizer/cases.py
@@ -9,7 +9,12 @@ keywords = ('__ENCODING__', '__LINE__', '__FILE__', 'BEGIN', 'END', 'alias', 'an
 operators = ('eql?', 'equal?', '==', '=', '!=', '<', '<=', '>', '>=', '+', '+', '-', '*', '/', '**', '==',
              '>', '<', '|', '&', '^', '<<', '[]', '===')
 
-punctuation = ('[', ']', '(', ')', ',', ';', '::', ':', '.', '{', '}', '\0')
+punctuation = ('[', ']', '(', ')', ',', ';', '.', '{', '}', '\0')
+
+annotations = {
+    'TokenType.METHOD': ['def', "#Annotation before def"],
+    'TokenType.CLASS': ['class', "#Annotation before class"]
+}
 
 
 def is_keyword(token):
@@ -33,23 +38,23 @@ def is_ruby_file(name):
 def get_list_from_line(line):
     for operator in operators:
         line = line.replace(operator, ' ' + operator + ' ')
-    # for punct in punctuation:
-    #     line = line.replace(punct, ' ' + punct + ' ')
+    for punct in punctuation:
+        line = line.replace(punct, ' ' + punct + ' ')
     line = re.sub("^(\s)+", " ", line)
     line = line.split(' ')
     line = list(filter(None, line))
     return line
 
 
-def get_string_from_list(lexemes):
-    return ''.join(lexemes)
+def get_file_from_list(list):
+    return '\n'.join(list)
 
 
 def diff(li1, li2):
     return list(list(set(li1) - set(li2)) + list(set(li2) - set(li1)))
 
 
-def get_file_names(f1, f2):
+def get_folder_names(f1, f2):
     f1 = f1.replace('\\', '/')
     f1 = f1.split('/')
     f2 = f2.replace('\\', '/')
